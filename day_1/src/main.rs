18abb4c12c11ea0593bb7ca23c659ee6509e0c35
Colin Lappala <colin@amperity.com>
2019-12-06 20:50:47 -0800

    🌟 day 1, part 2.

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,6 +1,18 @@
 use std::io::{self, BufRead};
+use std::cmp::Ordering;
 
-fn fuel_required(mass: f32) -> i32 {
+
+fn fuel_required_fuel(fuel_mass: i32) -> i32 {
+    let fuel_fuel_mass: i32 = (fuel_mass as f32 / 3_f32).floor() as i32 - 2;
+    // println!("fuel {}, extra {}", fuel_mass, fuel_fuel_mass);
+    return fuel_fuel_mass + match fuel_fuel_mass.cmp(&0) {
+        Ordering::Less => -1 * fuel_fuel_mass,
+        Ordering::Equal => 0i32,
+        Ordering::Greater => fuel_required_fuel(fuel_fuel_mass)
+    };
+}
+
+fn fuel_required_module(mass: f32) -> i32 {
     return (mass / 3_f32).floor() as i32 - 2;
 }
 
@@ -8,7 +20,10 @@ fn main() {
     let mut sum = 0;
     for line in io::stdin().lock().lines() {
         let mass = line.unwrap().parse::<f32>().expect("Parsing failed");
-        sum += fuel_required(mass);
+        let module_fuel_mass = fuel_required_module(mass);
+        let fuel_fuel_mass = fuel_required_fuel(module_fuel_mass);
+        //println!("module {}, fuel {}", module_fuel_mass, fuel_fuel_mass);
+        sum += module_fuel_mass + fuel_fuel_mass;
     }
     println!("{}", sum);
 }
