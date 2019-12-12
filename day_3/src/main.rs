699f1be528c233953eabdcf74abbb5641644eebe
Colin Lappala <colin@amperity.com>
2019-12-12 09:46:02 -0800

    🌟 day 3, part 1

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,4 +1,64 @@
-use std::io::{self, BufRead};
+use std::io;
+use std::i32::MAX;
+use std::collections::HashSet;
+
+fn get_wire_input() -> String {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("failed to read line");
+
+    return line;
+}
+
+fn make_wire((x, y): (i32, i32)) -> HashSet<(i32, i32)> {
+    let mut cur_x = x;
+    let mut cur_y = y;
+    let mut wire: HashSet<(i32, i32)> = HashSet::new();
+    for item in get_wire_input().split(',') {
+        let (dir, _count) = item.trim().split_at(1);
+        let count = _count.parse::<u32>().expect("Failed to parse step count");
+
+        match dir {
+            "R" => {
+                // go +count in x direction
+                let target = cur_x + count as i32;
+                while cur_x <= target {
+                    cur_x += 1;
+                    wire.insert((cur_x, cur_y));
+                    //println!("{}: {} <= {}", count, cur_x, (cur_x + count as i32));
+                };
+            },
+            "U" => {
+                // go +count in y direction
+                let target = cur_y + count as i32;
+                while cur_y <= target {
+                    cur_y += 1;
+                    wire.insert((cur_x, cur_y));
+                };
+            },
+            "D" => {
+                // go -count in y direction
+                let target = cur_y - count as i32;
+                while cur_y >= target {
+                    cur_y -= 1;
+                    wire.insert((cur_x, cur_y));
+                };
+            },
+            "L" => {
+                // go -count in x direction
+                let target = cur_x - count as i32;
+                while cur_x >= target {
+                    cur_x -= 1;
+                    wire.insert((cur_x, cur_y));
+                };
+            },
+            _ => {
+                panic!("Unknown direction {}", dir);
+            }
+        }
+    }
+
+    wire
+}
 
 fn main() {
     // 2 wires, described in input as stateful directions (eg R10, U2, D10) tracing the wires path in 2D space
@@ -16,7 +76,7 @@ fn main() {
     // 0. Setup
     //   - input is: one line per wire, comma-separated directions
     //   - per line:
-    //     * initialize a set of u32 2-tuples
+    //     * initialize a set of i32 2-tuples
     //     * initialize starting coordinate (0,0)
     //   - per comma-separated item:
     //     * parse the item into [direction, count]
@@ -25,7 +85,18 @@ fn main() {
     //   - perform a set intersection between the two sets
     // 2. Find the closest to origin
     //   - return the minimum from this intersection based on the sum of each tuple
-    for line in io::stdin().lock().lines() {
-        println!("{}", line.unwrap());
+    let first = make_wire((0,0));
+    println!("first wire is this long: {}", first.len());
+    let second = make_wire((0,0));
+    println!("second wire is this long: {}", second.len());
+
+    let mut cur_min = std::i32::MAX;
+    for (x, y) in first.intersection(&second) {
+        let distance = x.abs() + y.abs();
+        if cur_min > distance {
+            cur_min = distance;
+        }
     }
+
+    println!("min distance: {}", cur_min);
 }
